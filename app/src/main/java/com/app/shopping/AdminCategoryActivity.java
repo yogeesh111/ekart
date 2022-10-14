a08b6ec85ea95b32050058288aebf643e6023c38
YOGEESH <103254122+yogeesh111@users.noreply.github.com>
2022-10-14 09:53:11 +0530

    Add files via upload

---
+++ app/src/main/java/com/app/shopping/AdminCategoryActivity.java
@@ -0,0 +1,55 @@
+package com.app.shopping;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.view.View;
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminCategoryActivity extends AppCompatActivity {
+
+    private Button LogoutBtn,Addbtn, CheckOrdersBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_category);
+
+        Addbtn=(Button) findViewById(R.id.new_product);
+        Addbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(AdminCategoryActivity.this, com.app.shopping.AdminAddNewProductActivity.class);
+                intent.putExtra("category", "New-product");
+                startActivity(intent);
+            }
+        });
+
+
+        LogoutBtn = (Button) findViewById(R.id.admin_logout_btn);
+        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(AdminCategoryActivity.this,MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+        CheckOrdersBtn = (Button) findViewById(R.id.check_orders_btn);
+
+
+        CheckOrdersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(AdminCategoryActivity.this,AdminNewOrdersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
